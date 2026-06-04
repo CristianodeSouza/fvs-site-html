6d4309da225e64a11cd741438be36918edfdb7e1
KPG Imoveis <csrdesouza@gmail.com>
2026-06-04 04:16:06 -0300

    Clarify FVS method in practice

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ if (heroImages.length && !prefersReducedMotion) {
 }
 
 const revealItems = document.querySelectorAll(
-  '.manifesto__inner, .metodo-fvs__intro, .metodo-fvs__steps article, .gramado__media, .gramado__content, .experiencia__content, .experiencia__moment, .section-heading, .empreendimento, .projeto-detalhe__intro, .projeto-detalhe__layout, .projeto-detalhe__proofs figure, .projeto-detalhe__criteria article, .tipologias__intro, .tipologia-card, .decisao__intro, .decisao__card, .decisao__notes, .seguranca-decisao__intro, .seguranca-decisao__grid article, .galeria__intro, .galeria__item, .galeria-proof, .cta-consultivo .container'
+  '.manifesto__inner, .metodo-fvs__intro, .metodo-fvs__steps article, .metodo-fvs__proof, .gramado__media, .gramado__content, .experiencia__content, .experiencia__moment, .section-heading, .empreendimento, .projeto-detalhe__intro, .projeto-detalhe__layout, .projeto-detalhe__proofs figure, .projeto-detalhe__criteria article, .tipologias__intro, .tipologia-card, .decisao__intro, .decisao__card, .decisao__notes, .seguranca-decisao__intro, .seguranca-decisao__grid article, .galeria__intro, .galeria__item, .galeria-proof, .cta-consultivo .container'
 );
 
 revealItems.forEach((item) => {
